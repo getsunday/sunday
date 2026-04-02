63821d52a2937516583a19ee4bcae4323e923892
George Lemon <georgelemon@protonmail.com>
2026-04-02 15:55:56 +0300

    update base middleware
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

--- src/service/middleware/base.nim
+++ src/service/middleware/base.nim
@@ -5,25 +5,34 @@
 #     Made by Humans from OpenPeeps
 #     https://github.com/openpeeps/sunday
 
-import std/[strutils, times, options]
+import std/[strutils, times, options, os]
+
+import pkg/limiter
 import pkg/supranim/middleware
 import ../provider/session
 
-# import pkg/limiter
+var globalLimiter = Limiter(
+  maximumHits: 200,
+  timeLimit: initDuration(seconds = 20),
+  timeToWait: initDuration(seconds = 30)
+)
 
-# var globalLimiter = Limiter(
-#   maximumHits: 300,
-#   timeLimit: initDuration(seconds = 60),
-#   timeToWait: initDuration(seconds = 60)
-# )
-
-# newBaseMiddleware limitChecker:
-#   ## Checks if the request is coming from an IP
-#   ## address that is currently limited by the server
-#   when defined release:
-#     if not globalLimiter.hit(req.getIp):
-#       req.resp(code = HttpCode(429), "", res.getHeaders())
-#   else: discard
+newBaseMiddleware limitChecker:
+  ## Checks if the request is coming from an IP
+  ## address that is currently limited by the server
+  when defined release:
+    case globalLimiter.hit(req.getIp):
+    of LimitResult.lrLimited:
+      req.resp(code = HttpCode(429), "Too Many Requests", res.getHeaders)
+      return false
+    of LimitResult.lrTarpit:
+      # drop the request by closing the connection without sending a resp
+      req.dropRequest()
+      return false
+    of LimitResult.lrAllowed:
+      discard # next is not needed in a base middleware
+  else: discard
+  result = true
 
 newBaseMiddleware uriChecker:
   ## Fix the trailing slash in the URI
@@ -31,3 +40,5 @@ newBaseMiddleware uriChecker:
   if path != "/" and path[^1] == '/':
     res.addHeader("Location", path[0..^2])
     req.resp(code = HttpCode(301), "", res.getHeaders())
+    return false
+  result = true
